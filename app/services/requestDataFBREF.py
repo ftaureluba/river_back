15cee049c53f74aebc7f54b4f59346fc57045bc5
feli <ftaurel@fi.uba.ar>
2024-08-29 01:46:56 -0300

    bs4 funcionando bien para crear dict con data adicional

--- app/services/requestDataFBREF.py
+++ app/services/requestDataFBREF.py
@@ -14,19 +14,29 @@ def data():
         if tablas:
             coso = tablas.find_all('tr')
             for row in coso:
-                data = row.find_all(['th', 'td']) #tengo que buscar, en los que tiene, la clase 'data_original_value' porque sino me los da por 90 en vez del dato duro
+                data = row.find_all(['th', 'td']) 
                 cell_data = [celda.get_text(strip=True) for celda in data]
                 datos_posta.append(cell_data)
                 
-
+        
         dict_datos = []
-        coso = {}
-        for i in range (2, len(datos_posta) - 1):
+        
+        for i in range (2, len(datos_posta)):
+            coso = {}
             for j in range(0, len(datos_posta[1]) - 1):
-              coso[datos_posta[1][j]] = datos_posta[i][j]
-            print(coso)
+                if datos_posta[1][j] in coso:
+                  aux = datos_posta[1][j]
+                  coso[f'{aux} per 90'] = datos_posta[i][j]
+                  #coso[datos_posta[1][j]] = datos_posta[i][j]
+                else:
+                  coso[datos_posta[1][j]] = datos_posta[i][j]
+            
             dict_datos.append(coso)
-
+        print(dict_datos)
+        
     else: 
         print(f'error en el request {res.status_code}')
     return
+
+#FIJARSE QUE ME ESTA DANDO LOS DATOS POR 90, PERO PORQUE ESTA PISANDO LOS DATOS DUROS
+                
